14d326dbec31b56e28431a3a7fe0dbafb32655b7
duydn1611 <duydn1611@gmail.com>
2018-11-01 15:09:59 +0700

    duydn commit

--- vietstem_app/src/pages/course/course.ts
+++ vietstem_app/src/pages/course/course.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the CoursePage page.
@@ -15,11 +15,65 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CoursePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CoursePage');
   }
-
+  activeCourse() {
+    let prompt = this.alertCtrl.create({
+      title: 'Kích hoạt khóa học',
+      message: "Để kích hoạt khóa học, vui lòng nhập mã kích hoạt",
+      cssClass: 'alertCss',
+      inputs: [
+        {
+          name: 'activecode',
+          placeholder: 'Nhập mã kích hoạt'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Gửi',
+          cssClass: 'sent-css',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Quên mã kích hoạt',
+          handler: () => {            
+            let promptForgot = this.alertCtrl.create({
+              title: 'Quên mã kích hoạt',
+              message: "Vui lòng nhập số điện thoại hoặc email bạn đã đăng ký khóa học",
+              cssClass: 'alertCss',
+              inputs: [
+                {
+                  name: 'activecode',
+                  placeholder: 'Nhập số điện thoại hoặc email'
+                },
+              ],
+              buttons: [
+                {
+                  text: 'Gửi',
+                  cssClass: 'sent-css',
+                  handler: data => {
+                    console.log('Cancel clicked');
+                  }
+                }
+              ],
+            });
+            promptForgot.present();
+          }
+        },
+        {
+          text: 'Đăng ký mua',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ],
+    });
+    prompt.present();
+  }
 }
